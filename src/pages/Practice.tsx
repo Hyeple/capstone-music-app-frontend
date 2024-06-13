46eb91b3d94aa3c7784b6d78e649f8bff2108082
Hyeple <hyzaar1@gmail.com>
2024-06-14 05:16:33 +0900

    Solve error

--- src/pages/Practice.tsx
+++ src/pages/Practice.tsx
@@ -1,28 +1,57 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { useLocation } from 'react-router-dom';
 import axios from 'axios';
 import { OpenSheetMusicDisplay } from 'opensheetmusicdisplay';
 import AudioPlayer from 'osmd-audio-player';
-import { FaFileUpload, FaRedo } from 'react-icons/fa';
 import { Modal } from 'react-responsive-modal';
-import 'react-responsive-modal/styles.css';
 
-const Practice = ({ initialXmlData = '' }) => {
-  const fileInputRef = useRef<HTMLInputElement>(null);
+const Practice = () => {
+  const location = useLocation();
+  const initialXmlData = location.state?.initialXmlData || '';
   const [xmlData, setXmlData] = useState<string>(initialXmlData);
   const [bpm, setBpm] = useState<number>(100);
   const [key, setKey] = useState<string>('0');
   const [fileSelected, setFileSelected] = useState<boolean>(!!initialXmlData);
-  const [model, setModel] = useState<string>('4stems');
-  const [instrumentType, setInstrumentType] = useState<string>('guitar');
+  const [loading, setLoading] = useState<boolean>(!initialXmlData);
   const [open, setOpen] = useState<boolean>(false);
   const [scoreData, setScoreData] = useState<{ score: number, incorrectParts: string[] }>({ score: 0, incorrectParts: [] });
   const osmdRef = useRef<OpenSheetMusicDisplay | null>(null);
   const audioPlayer = useRef<AudioPlayer | null>(null);
 
-  const exampleScoreData = {
-    score: 85,
-    incorrectParts: ['Measure 2, Beat 3', 'Measure 5, Beat 1']
-  };
+  const dummyXml = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+    <score-partwise version="3.1">
+      <part-list>
+        <score-part id="P1">
+          <part-name>Music</part-name>
+        </score-part>
+      </part-list>
+      <part id="P1">
+        <measure number="1">
+          <attributes>
+            <divisions>1</divisions>
+            <key>
+              <fifths>0</fifths>
+            </key>
+            <time>
+              <beats>4</beats>
+              <beat-type>4</beat-type>
+            </time>
+            <clef>
+              <sign>G</sign>
+              <line>2</line>
+            </clef>
+          </attributes>
+          <note>
+            <pitch>
+              <step>C</step>
+              <octave>4</octave>
+            </pitch>
+            <duration>4</duration>
+            <type>whole</type>
+          </note>
+        </measure>
+      </part>
+    </score-partwise>`;
 
   useEffect(() => {
     audioPlayer.current = new AudioPlayer();
@@ -31,6 +60,14 @@ const Practice = ({ initialXmlData = '' }) => {
   useEffect(() => {
     if (initialXmlData) {
       initSheet(initialXmlData);
+      setLoading(false);
+    } else {
+      setLoading(true);
+      setTimeout(() => {
+        setXmlData(dummyXml);
+        initSheet(dummyXml);
+        setLoading(false);
+      }, 2000);
     }
   }, [initialXmlData]);
 
@@ -78,129 +115,24 @@ const Practice = ({ initialXmlData = '' }) => {
     }
   };
 
-  const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
-    const files = event.target.files;
-    if (files && files[0]) {
-      const formData = new FormData();
-      formData.append("file", files[0]);
-      formData.append("model", model);
-      formData.append('instrumentType', instrumentType);
-      const token = localStorage.getItem("token");
-
-      try {
-        const response = await axios.post('/api/ml/separate', formData, {
-          headers: {
-            "Content-Type": "multipart/form-data",
-            "Authorization": `Bearer ${token}`
-          }
-        });
-        console.log(response.data);
-        setFileSelected(true);
-
-        // 파일 업로드 후 XML 데이터를 가져오는 요청
-        const xmlResponse = await axios.get('/api/ml/get_xml', {
-          headers: {
-            "Authorization": `Bearer ${token}`
-          }
-        });
-        console.log("XML response received:", xmlResponse.data);
-        await initSheet(xmlResponse.data);
-
-      } catch (error) {
-        console.error("Error uploading file or fetching XML data:", error);
-      }
-    }
-  };
-
-  const handleChangeBpm = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newBpm = parseInt(event.target.value, 10) || 100;
-    setBpm(newBpm);
-    audioPlayer.current?.setBpm(newBpm);
-  };
-
-  const handleChangeKey = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const newKey = event.target.value;
-    const parser = new DOMParser();
-    const xmlDoc = parser.parseFromString(xmlData, 'application/xml');
-    xmlDoc.getElementsByTagName('fifths')[0].textContent = newKey;
-    const newXmlData = new XMLSerializer().serializeToString(xmlDoc);
-    initSheet(newXmlData);
-    setKey(newKey);
-  };
-
-  const handleChangeModel = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setModel(event.target.value);
-    if (event.target.value === '4stems' && instrumentType === 'piano') {
-      setInstrumentType('guitar');
-    }
-  };
-
-  const handleChangeInstrumentType = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setInstrumentType(event.target.value);
-  };
-
-  const handleCheckScore = async () => {
-    try {
-      const response = await axios.get('/api/getScore', {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('token')}`
-        }
-      });
-      const data = response.data || exampleScoreData;
-      setScoreData(data);
-      setOpen(true);
-    } catch (error) {
-      console.error('Error fetching score data:', error);
-      setScoreData(exampleScoreData);
-      setOpen(true);
-    }
-  };
-
   return (
     <div className="bg-gray500 p-4 h-screen flex flex-col items-center justify-center">
-      {!fileSelected && !initialXmlData && (
-        <div className="absolute inset-0 flex flex-col items-center justify-center bg-gray-500 z-10">
-          <label className="cursor-pointer flex flex-col items-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            <FaFileUpload className="text-3xl mb-2" />
-            Select Music File
-            <input type="file" ref={fileInputRef} onChange={handleFileChange} className="hidden" />
-          </label>
-          <div className="flex flex-col items-center mt-4">
-            <label className="text-white mb-2">Select Model:</label>
-            <select value={model} onChange={handleChangeModel} className="text-black mb-4">
-              <option value="4stems">4stems</option>
-              <option value="5stems">5stems</option>
-            </select>
-            <label className="text-white mb-2">Select Instrument Type:</label>
-            <select value={instrumentType} onChange={handleChangeInstrumentType} className="text-black">
-              <option value="guitar">Guitar</option>
-              <option value="base">Base</option>
-              <option value="vocal">Vocal</option>
-              <option value="drum">Drum</option>
-              {model === '5stems' && <option value="piano">Piano</option>}
-            </select>
-            <div className="mt-4 flex items-center">
-              <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded" onClick={handleCheckScore}>Check Score</button>
-            </div>
-          </div>
+      {loading ? (
+        <div className="flex items-center justify-center">
+          <div className="loader">Loading...</div>
         </div>
-      )}
-      {(fileSelected || initialXmlData) && (
+      ) : (
         <>
           <div className="flex justify-around items-center mt-4">
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => audioPlayer.current?.play()}>Play</button>
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => audioPlayer.current?.pause()}>Pause</button>
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => audioPlayer.current?.stop()}>Stop</button>
-            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" onClick={() => setFileSelected(false)}>
-              <FaRedo className="inline-block mr-2" />
-              Change File
-            </button>
           </div>
           <div className="mt-4 flex items-center">
             <span className="text-white mr-2">bpm:</span>
-            <input type="number" value={bpm} onChange={handleChangeBpm} className="text-black" />
+            <input type="number" value={bpm} onChange={(event) => setBpm(parseInt(event.target.value, 10) || 100)} className="text-black" />
             <span className="text-white ml-4 mr-2">key:</span>
-            <select value={key} onChange={handleChangeKey} className="ml-2 text-black">
+            <select value={key} onChange={(event) => setKey(event.target.value)} className="ml-2 text-black">
               <option value="-3">-3</option>
               <option value="-2">-2</option>
               <option value="-1">-1</option>
@@ -211,7 +143,7 @@ const Practice = ({ initialXmlData = '' }) => {
             </select>
           </div>
           <div className="mt-4 flex items-center">
-            <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded" onClick={handleCheckScore}>Check Score</button>
+            <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded" onClick={() => setOpen(true)}>Check Score</button>
           </div>
           <div id="score" className="mt-4 w-full h-full"></div>
         </>
